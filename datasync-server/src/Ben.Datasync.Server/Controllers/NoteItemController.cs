190953ebd905c8c3417c39d6eaa283d2c2a88789
Tim Edalatpour <edalatpour@hotmail.com>
2026-02-26 10:34:48 -0500

    Adding Datasync.Server project and infra files to Ben solution.

---
+++ datasync-server/src/Ben.Datasync.Server/Controllers/NoteItemController.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Datasync.Server;
+using CommunityToolkit.Datasync.Server.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Datasync.Server.Db;
+
+namespace Sample.Datasync.Server.Controllers;
+
+[Route("tables/[controller]")]
+[ApiExplorerSettings(IgnoreApi = false)]
+public class NoteItemController : TableController<NoteItem>
+{
+    public NoteItemController(AppDbContext context) 
+        : base(new EntityTableRepository<NoteItem>(context))
+    {
+    }
+}
---
+++ datasync-server/src/Ben.Datasync.Server/Db/NoteItem.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Datasync.Server.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample.Datasync.Server.Db;
+
+public class NoteItem : EntityTableData
+{
+    public DateTime Key { get; set; }
+
+    public int Order { get; set; }
+
+    [Required, MinLength(1)]
+    public string Text { get; set; } = string.Empty;
+    
+}
