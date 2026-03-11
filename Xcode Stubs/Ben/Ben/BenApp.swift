5c96a0dfd0b3bdac616d8a58b4b46e005c70663d
Tim Edalatpour <tim.edalatpour@outlook.com>
2026-03-10 20:15:45 -0400

    Updated icons and splash screen

---
+++ Xcode Stubs/Ben/Ben/BenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenApp.swift
+//  Ben
+//
+//  Created by Tim Edalatpour on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
